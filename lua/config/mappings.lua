f0f7eb36b3bda9279bdd7134d74bb0df8cb70c8c
mallie-su <matthieu.allie@cooperative-u.fr>
2026-06-29 12:36:03 +0200

    feat(mappings): disable arrow keys

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -5,3 +5,11 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+
+-- Disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move left!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move right!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move up!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move up!"<CR>')
+
