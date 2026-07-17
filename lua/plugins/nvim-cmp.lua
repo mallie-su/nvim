3ef907c3fd77f4f0b377eedd980468fd6b836e51
mallie-su <matthieu.allie@cooperative-u.fr>
2026-07-17 08:36:02 +0200

    feat: add cmp-copilot

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -6,6 +6,7 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
+    "hrsh7th/cmp-copilot",
   },
   -- Not all LSP servers add brackets when completing a function.
   -- To better deal with this, LazyVim adds a custom option to cmp,
@@ -44,6 +45,7 @@ return {
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
+        { name = "copilot" },
         { name = "path" },
       }, {
         { name = "buffer" },
